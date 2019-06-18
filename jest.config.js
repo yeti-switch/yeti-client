9c26b8098b998fd35b1476fb7f374ba7f3929e1b
sergey <serrikk@gmail.com>
2019-06-18 23:56:45 +0300

    added jest and sample test

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'moduleFileExtensions': [
+    'js',
+    'json',
+    'vue'
+  ],
+  'transform': {
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
+  }
+}
---
+++ tests/accounts.test.js
@@ -0,0 +1,38 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Accounts from '@/components/accounts/Accounts.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('Accounts.vue', () => {
+  let actions
+  let store
+  let state
+
+  beforeEach(() => {
+    state = {
+      requestPending: false,
+      accounts: {
+        data: 'somedata'
+      }
+    }
+    actions = {
+      getAccounts: jest.fn()
+    }
+    store = new Vuex.Store({
+      modules: {
+        accounts: {
+          state,
+          store,
+          actions,
+        }
+      }
+    })
+  })
+
+  it('is instance of Vue', () => {
+    const wrapper = shallowMount(Accounts, { store, localVue })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})

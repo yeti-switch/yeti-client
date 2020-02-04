3e10ce251cb8682ac4cd0dcedd943af60d7b6bfe
Andrii Miroshnychenko <Andrii Miroshnychenko>
2020-02-04 12:49:54 +0100

    time range filter exluded to the top

---
+++ src/components/ViewFilters/ViewFilters.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="view-filters">
+    <span>Accounts dropdown</span>
+    <TimeRangeFilter />
+  </div>
+</template>
+
+<script>
+import TimeRangeFilter from '../TimeRangeFilter/TimeRangeFilter';
+
+export default {
+  components: {
+    TimeRangeFilter,
+  },
+};
+</script>
+
+<style lang="scss">
+  .view-filters {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>

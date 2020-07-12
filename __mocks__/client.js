51403bc61e9e5424413bad7a5d5a4e59ace54b61
Andrii Miroshnychenko <andrew.miroshnichenko@gmail.com>
2020-07-12 13:05:09 +0200

    WIP: jsdom cleanup

--- __mocks__/client.js
+++ __mocks__/client.js
@@ -1,8 +1,2 @@
-import { JSDOM } from 'jsdom';
-
-const dom = new JSDOM();
-global.document = dom.window.document;
-global.window = dom.window;
-
 // Mocking document height check
 global.document.querySelector = () => ({ cllientHeight: 100 });

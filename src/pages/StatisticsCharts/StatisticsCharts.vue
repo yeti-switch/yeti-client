6edbc9e8ac0de8a5da55ea8d646a00ae66e7b256
Andrii Miroshnychenko <andrew.miroshnichenko@gmail.com>
2020-07-29 23:22:43 +0200

    WIP: cleanup of statistics charts

--- src/pages/StatisticsCharts/StatisticsCharts.vue
+++ src/pages/StatisticsCharts/StatisticsCharts.vue
@@ -39,63 +39,6 @@ export default {
   },
   computed: {
     ...mapGetters(['requestIsPending', 'activeAccount', 'activeCalls', 'originatedCps']),
-    derivedActiveCallsChartData() {
-      const chartData = {
-        datasets: [],
-      };
-
-      if (this.activeCalls) {
-        // For some reason, pushing dataset configs in scope of below Object.keys
-        // cause continous dataset refresh, which results in page crash
-        // chartData.datasets.push({
-        //   label: 'Originated calls',
-        //   data: [],
-        //   backgroundColor: 'transparent',
-        //   borderColor: 'lightgreen',
-        // });
-        // chartData.datasets.push({
-        //   label: 'Terminated calls',
-        //   data: [],
-        //   backgroundColor: 'transparent',
-        //   borderColor: 'lightblue',
-        // });
-
-        Object.keys(INITIAL_DATASETS_SETTINGS).forEach((key) => {
-          const dataset = { ...INITIAL_DATASETS_SETTINGS[key] };
-          dataset.data = this.activeCalls[key].map(({ x, y }) =>
-            ({ y: x, x: Date.parse(y) }));
-          chartData.datasets.push(dataset);
-          // this.activeCalls[key].forEach((dataEntry) => {
-          //   const { x, y } = dataEntry;
-
-          //   chartData.datasets[index].data.push({
-          //     y, x: Date.parse(x),
-          //   });
-          // });
-        });
-      }
-
-      return chartData;
-    },
-    derivedOriginatedCpsChartData() {
-      const chartData = {
-        datasets: [],
-      };
-
-      if (this.originatedCps) {
-        // For some reason, pushing dataset configs in scope of below Object.keys
-        // cause continous dataset refresh, which results in page crash
-        chartData.datasets.push({
-          label: 'Originated CPS',
-          data: this.originatedCps.cps.map(({ x, y }) =>
-            ({ y: x, x: Date.parse(y) })),
-          backgroundColor: 'transparent',
-          borderColor: 'orange',
-        });
-      }
-
-      return chartData;
-    },
     derivedCharData() {
       const chartsData = { ...this.activeCalls, ...this.originatedCps };
 

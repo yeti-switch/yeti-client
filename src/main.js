731998341399061c0ec045caadf2d36d91dfc13d
Andrii Miroshnychenko <andrew.miroshnichenko@gmail.com>
2020-08-15 10:36:09 +0200

    WIP: package added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import { sync } from 'vuex-router-sync';
 import VueRouter from 'vue-router';
 import AntVue from 'ant-design-vue';
+import VueI18n from 'vue-i18n';
 import Notifications from 'vue-notification';
 import 'ant-design-vue/dist/antd.min.css';
 
@@ -12,6 +13,7 @@ import store from './store/store';
 Vue.use(VueRouter);
 Vue.use(AntVue);
 Vue.use(Notifications);
+Vue.use(VueI18n);
 
 Vue.config.productionTip = false;
 

729598221bd81b0c490b99b8fb5e769972fc6263
Andrii Miroshnychenko <andrew.miroshnichenko@gmail.com>
2020-08-30 22:51:19 +0200

    WIP: tests updated and written for a new helper

--- src/pages/Accounts/tests/Accounts.test.js
+++ src/pages/Accounts/tests/Accounts.test.js
@@ -1,5 +1,6 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
+import VueI18n from 'vue-i18n';
 
 import { ACCOUNT_DETAILS, COMMON_TABLE_ENTITY_EXCLUDED_FIELDS } from '@/constants';
 import VerticalListAnt from '@/components/VerticalListAnt/VerticalListAnt';
@@ -7,6 +8,8 @@ import Accounts from '../Accounts.vue';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(VueI18n);
+const i18n = new VueI18n({ locale: 'en' });
 
 describe('Accounts page', () => {
   it('is instance of Vue, with no account entries in store', () => {
@@ -25,7 +28,7 @@ describe('Accounts page', () => {
         },
       },
     });
-    const wrapper = shallowMount(Accounts, { store, localVue });
+    const wrapper = shallowMount(Accounts, { store, localVue, i18n });
 
     expect(wrapper.findComponent(VerticalListAnt).exists()).toBeTruthy();
     expect(getAccountDetails).toHaveBeenCalled();
@@ -48,7 +51,7 @@ describe('Accounts page', () => {
       },
     });
 
-    const wrapper = shallowMount(Accounts, { store, localVue });
+    const wrapper = shallowMount(Accounts, { store, localVue, i18n });
     const numberOfRowsInAccountInfo = Object.keys(ACCOUNT_DETAILS).length
     - COMMON_TABLE_ENTITY_EXCLUDED_FIELDS.length;
 

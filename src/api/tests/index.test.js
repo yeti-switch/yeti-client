e3272da71559cc06e0a600d75cf8ad526c29f4c3
Andrii Miroshnychenko <andrew.miroshnichenko@gmail.com>
2020-12-31 14:39:24 +0100

    one more expect added

--- src/api/tests/index.test.js
+++ src/api/tests/index.test.js
@@ -1,12 +1,16 @@
+import Api from 'devour-client';
 import { RESOURCES } from '@/constants';
 import JsonApi from '../index';
 
 describe('JsonApi', () => {
-  it('will be initialized in proper order', () => {
+  it('will be initialized properly', () => {
     const addRelationshipSpy = jest.spyOn(JsonApi.prototype, 'addRelationship');
     const numberOfRelationships = Object.values(RESOURCES).length;
 
     expect(JsonApi.apiInstance).toBeInstanceOf(JsonApi);
+    // TODO in this assert we can see that naming and service init
+    // aren't the best
+    expect(JsonApi.apiInstance.instance).toBeInstanceOf(Api);
 
     expect(addRelationshipSpy).toHaveBeenCalledTimes(numberOfRelationships);
     addRelationshipSpy.mockRestore();
